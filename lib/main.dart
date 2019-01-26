2ed2bb90f4f3c432d6972110694628bd3d2ac53c
Opipi <koojj2809571@iCloud.com>
2019-01-26 21:52:42 +0800

    创建版本库

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(CopyOSCApp());
+
+class CopyOSCApp extends StatefulWidget{
+
+  @override
+  CopyOSCAppState createState() => CopyOSCAppState();
+
+}
+
+class CopyOSCAppState extends State<CopyOSCApp>{
+  final appBarTitles = ['资讯', '动弹', '发现', '我的'];
+  final titleTextStyleSelected = TextStyle(color: const Color(0xff63ca6c));
+  final titleTextStyleNormal = TextStyle(color: const Color(0xff969696));
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+}
